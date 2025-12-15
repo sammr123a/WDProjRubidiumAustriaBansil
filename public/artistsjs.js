db818e5aaf131bcec21fce1f0cb3989c02d44659
emj-bans <b2029erbansil@pshs.edu.ph>
2025-12-15 22:04:09 +0800

    Major Edits 9

--- public/artistsjs.js
+++ public/artistsjs.js
@@ -8,15 +8,6 @@ tupacbtn.onclick = function() {
   tupacmodal.style.display = "block";
 }
 
-span.onclick = function() {
-  tupacmodal.style.display = "none";
-}
-
-window.onclick = function(event) {
-  if (event.target == tupacmodal) {
-    tupacmodal.style.display = "none";
-  }
-}
 
 var snoopmodal = document.getElementById("snoop-modal");
 
@@ -28,16 +19,6 @@ snoopbtn.onclick = function() {
   snoopmodal.style.display = "block";
 }
 
-snoopspan.onclick = function() {
-  snoopmodal.style.display = "none";
-}
-
-window.onclick = function(event) {
-  if (event.target == snoopmodal) {
-    snoopmodal.style.display = "none";
-  }
-}
-
 
 var dremodal = document.getElementById("dre-modal");
 
@@ -45,16 +26,77 @@ var drebtn = document.getElementById("dre-modal-btn");
 
 var drespan = document.getElementsByClassName("dreclose")[0];
 
-drespan.onclick = function() {
+drebtn.onclick = function() {
   dremodal.style.display = "block";
 }
 
-snoopspan.onclick = function() {
-  dremodal.style.display = "none";
+
+var eazyemodal = document.getElementById("eazye-modal");
+
+var eazyebtn = document.getElementById("eazye-modal-btn");
+
+var eazyespan = document.getElementsByClassName("eazyeclose")[0];
+
+eazyebtn.onclick = function() {
+  eazyemodal.style.display = "block";
 }
 
-window.onclick = function(event) {
-  if (event.target == dremodal) {
-    dremodal.style.display = "none";
-  }
+var jayzmodal = document.getElementById("jayz-modal");
+
+var jayzbtn = document.getElementById("jayz-modal-btn");
+
+var jayzspan = document.getElementsByClassName("jayzclose")[0];
+
+jayzbtn.onclick = function() {
+  jayzmodal.style.display = "block";
 }
+
+
+var bigmodal = document.getElementById("big-modal");
+
+var bigbtn = document.getElementById("big-modal-btn");
+
+var bigspan = document.getElementsByClassName("bigclose")[0];
+
+bigbtn.onclick = function() {
+  bigmodal.style.display = "block";
+}
+
+
+var nasmodal = document.getElementById("nas-modal");
+
+var nasbtn = document.getElementById("nas-modal-btn");
+
+var nasspan = document.getElementsByClassName("nasclose")[0];
+
+nasbtn.onclick = function() {
+  nasmodal.style.display = "block";
+}
+
+
+var fiftymodal = document.getElementById("fifty-modal");
+
+var fiftybtn = document.getElementById("fifty-modal-btn");
+
+var fiftyspan = document.getElementsByClassName("fiftyclose")[0];
+
+fiftybtn.onclick = function() {
+  fiftymodal.style.display = "block";
+}
+
+
+
+window.onclick = function(event) {
+  if (event.target === tupacmodal) tupacmodal.style.display = "none";
+  if (event.target === snoopmodal) snoopmodal.style.display = "none";
+  if (event.target === dremodal) dremodal.style.display = "none";
+  if (event.target === eazyemodal) dremodal.style.display = "none";
+  if (event.target === jayzmodal) tupacmodal.style.display = "none";
+  if (event.target === bigmodal) snoopmodal.style.display = "none";
+  if (event.target === nasmodal) dremodal.style.display = "none";
+  if (event.target === fiftymodal) dremodal.style.display = "none";
+}
+
+document.querySelectorAll(".close, .snoopclose, .dreclose, .eazyeclose, .jayzclose, .bigclose, .nasclose, .fiftyclose").forEach(btn => {
+  btn.onclick = () => btn.closest(".modal").style.display = "none";
+});
