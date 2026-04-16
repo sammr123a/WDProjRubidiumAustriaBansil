3799fef701b98bf5749f4f6742d38a7835434185
sammr123a <b2029stkaustria@pshs.edu.ph>
2026-04-16 22:56:52 +0800

    fram of photobooth strip

--- public/about-usjs.js
+++ public/about-usjs.js
@@ -30,3 +30,32 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 });
+
+/*photobooth*/
+const elements = {
+  video: document.getElementById('liveVideo'),
+  canvas: document.getElementById('finalCanvas'),
+  ctx: document.getElementById('finalCanvas').getContext('2d'),
+  takePhotoBtn: document.getElementById('takePhoto'),
+  countdownEl: document.querySelector('.countdown-timer')
+};
+
+let photoStage = 0;
+
+const countDown = callback => {
+  let count = 3;
+  const countdownEl = elements.countdownEl;
+  countdownEl.textContent = count;
+  countdownEl.style.display = 'flex';
+
+  const interval = setInterval(() => {
+    count--;
+    if (count > 0) {
+      countdownEl.textContent = count;
+    } else {
+      clearInterval(interval);
+      countdownEl.style.display = 'none';
+      callback();
+    }
+}
+}
